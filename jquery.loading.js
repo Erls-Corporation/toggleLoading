81b16f4fdb01527b75efd5f1e54111a87a2c1ae5
Oliver Joseph Ash <oliverash@me.com>
2011-12-11 12:32:46 +0000

    Change fixed positioning to absolute on spinner

--- jquery.loading.js
+++ jquery.loading.js
@@ -1,67 +1,69 @@
 (function ($) {
 	$.fn.toggleLoading = function (config) {
-		var options = {
-			dim: 1 / 3,
-			width: 42,
-			height: 42,
-			opacity: 3 / 4,
-			color: 'light',
-			image: 'images/spinner.png',
-			speed: 75
-		};
+		var image,
+			options = {
+				dim: 2 / 3,
+				width: 42,
+				height: 42,
+				opacity: 3 / 4,
+				color: 'light',
+				imagesFolder: '../images',
+				speed: 75
+			};
 		
 		if (config) {
 			$.extend(options, config);
+			
+			if (options.dim === false) {
+				options.dim = '';
+			}
 		}
 		
-		if (options.color === 'dark') {
-			options.image = 'images/spinner-white.png';
-		}
+		image = options.imagesFolder + '/' + ((options.color === 'dark') ? 'spinner-white.png' : 'spinner.png');
 		
 		return this.each(function () {
 			var $this = $(this),
-				loading;
-			
-			loading = $this.data('loading');
+				$contents = $this.children(),
+				loading = $this.data('loading');
 			
 			if (!loading) {
-				$this
-					.css('opacity', options.dim)
-					.data('loading', {
-						spinner: $(document.createElement('div'))
-							.css({
-								'position': 'fixed',
-								'z-index': 1,
-								'width': options.width,
-								'height': options.height,
-								'opacity': options.opacity,
-								'background-image': 'url(' + options.image + ')'
-							})
-							.addClass('spinner')
-							.data('color', options.color)
-							.insertBefore(this),
-						interval: setInterval(function () {
-							loading.spinner.css({
-								'background-position-y': parseInt(loading.spinner.css('background-position-y'), 10) - options.height
-							});
-						}, options.speed)
-					});
+				$this.data('loading', {
+					spinner: $(document.createElement('div'))
+						.css({
+							'position': 'absolute',
+							'z-index': 1,
+							'width': options.width,
+							'height': options.height,
+							'opacity': options.opacity,
+							'background-image': 'url(' + image + ')'
+						})
+						.addClass('spinner')
+						.data('color', options.color)
+						.insertBefore(this),
+					interval: setInterval(function () {
+						loading.spinner.css({
+							'background-position-y': parseInt(loading.spinner.css('background-position-y'), 10) - options.height
+						});
+					}, options.speed)
+				});
+				
+				$contents.css('opacity', options.dim);
 				
 				loading = $this.data('loading');
 				
 				$(window).resize(function () {
 					loading.spinner.css({
-						'top': $this.offset().top + ($this.height() / 2) - (options.height / 2),
-						'left': $this.offset().left + ($this.width() / 2) - (options.width / 2)
+						'top': $this.position().top + ($this.height() / 2) - (options.height / 2),
+						'left': $this.position().left + ($this.width() / 2) - (options.width / 2)
 					});
 				}).resize();
 			} else {
 				loading.spinner.remove();
 				clearInterval(loading.interval);
 				
-				$this
-					.css('opacity', '')
-					.removeData('loading');
+				$contents.css('opacity', '');
+				
+				$this.removeData('loading');
 			}
 			
 			return this;
